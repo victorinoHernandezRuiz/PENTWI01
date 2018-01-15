19f9357676f06eae4a56f7a7d2d9a8940e331cb6
Victorino Hernandez <victorino.hernandez@softtek.com>
2018-01-15 13:52:22 -0600

    se realiza el commit de la carpeta de webcontent

---
+++ WebContent/js/scripts/scriptCore.js
@@ -0,0 +1,13 @@
+/**
+ * Script que contiene todos las funciones utilizadas en este ejemplo 
+ * @author victorino.hernandez 
+ * @date Enero de 2018
+ * @version 150120181213 
+ */
+
+/**
+ * Metodo que se ejecuta al cargarse la pagina inicial
+ */
+$(document).ready(function(){
+    
+})

3aaeb728e1c55da8444f526c0829e0faaf81906b
Victorino Hernandez <victorino.hernandez@softtek.com>
2018-01-15 18:58:42 -0600

    Se suben elementos para seleccion de componentes por atributo

--- WebContent/js/scripts/scriptCore.js
+++ WebContent/js/scripts/scriptCore.js
@@ -9,5 +9,15 @@
  * Metodo que se ejecuta al cargarse la pagina inicial
  */
 $(document).ready(function(){
-    
+   $("#buttonValidar").on("click",function(){
+       var nombre = $("#inputAtributo").val();
+       var valor = $("#inputValor").val();
+       seleccionPorAtributo(nombre,valor);
+   });
 })
+
+function seleccionPorAtributo(nombre, valor){
+    $("#mensajes").text("Se buscan los elementos de tipo input que tengan el atributo: "+nombre+"='"+valor+"'");
+    var objects =$("input["+nombre+"='"+valor+"']");
+    $("#mensajes").append("  Se obtuvieron:"+objects.length+" elementos");
+}
